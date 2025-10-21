60b82f7d7c1a0dc9d52e27398176e5bcf685936c
thefranklincode <you@example.com>
2025-10-21 15:50:56 +0530

    dommy link

---
+++ script.js
@@ -0,0 +1,23 @@
+// function makeFrirdRaice() {
+//   var friedRaciAmmount = "100rs"
+//   if (true) {
+//     let bill = "100rs recived from phonepe"
+//     const delivery = "fried rice delivery to the customer"
+//     console.log(friedRaciAmmount);
+//     console.log(bill);
+//     console.log(delivery)
+//   }
+
+// }
+// makeFrirdRaice();
+
+const friendName = "Sasi Kumar";
+const age = 24;
+// function Wishes() {
+//   if (true) {
+//     console.log("Happy Birthday " + friendName + " Now you are " + age + "years old");
+//   }
+// }
+// Wishes();
+console.log(`Happy birthday ${friendName}, Now you are ${age} years old`);
+
